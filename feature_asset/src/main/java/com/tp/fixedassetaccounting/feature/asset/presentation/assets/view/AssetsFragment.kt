97ae11fe2fed1962b6adbb6138ba16d78f9afde9
miel3k <kuba.mielczarek96@icloud.com>
2020-04-18 11:21:58 +0200

    Link assets fragment and new asset fragment

--- feature_asset/src/main/java/com/tp/fixedassetaccounting/feature/asset/presentation/assets/view/AssetsFragment.kt
+++ feature_asset/src/main/java/com/tp/fixedassetaccounting/feature/asset/presentation/assets/view/AssetsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tp.feature_asset.R
 import com.tp.fixedassetaccounting.core.extension.observe
@@ -37,6 +38,7 @@ class AssetsFragment : Fragment(), KodeinAware {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        setupFloatingActionButton()
         setupStateObserver()
         viewModel.loadData()
     }
@@ -45,6 +47,13 @@ class AssetsFragment : Fragment(), KodeinAware {
         rv_assets.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = assetsAdapter
+        }
+    }
+
+    private fun setupFloatingActionButton() {
+        fab_new_asset.setOnClickListener {
+            val navDirections = AssetsFragmentDirections.openNewAssetFragment()
+            findNavController().navigate(navDirections)
         }
     }
 
